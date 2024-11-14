dc047855f4560598dc304ed2c10948b81a16776f
choskin1 <134260042+choskin1@users.noreply.github.com>
2024-11-14 16:05:18 +1300

    trying to fix url routing

--- PropertyApplication/urls.py
+++ PropertyApplication/urls.py
@@ -4,7 +4,7 @@ from properties.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # This line enables the admin interface at /admin
-    path('api/', include('properties.urls')),  # Ensure your app URLs are included as well
+    path('properties/',include('properties.urls')),  # Ensure your app URLs are included as well
     path('', home, name = 'home'), # Route for the home page
 
 ]
